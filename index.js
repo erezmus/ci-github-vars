6eeb087582f6b51319963fce76e989be6e7fe775
Erez Odier <2045191+erezmus@users.noreply.github.com>
2023-01-26 10:39:24 +0000

    show var names

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-console.log(process.env.GITHUB_REF);
-console.log(process.env.GITHUB_BASE_REF);
-console.log(process.env.GITHUB_HEAD_REF);
-console.log(process.env.GITHUB_REF_NAME);
+console.log('GITHUB_REF: ', process.env.GITHUB_REF);
+console.log('GITHUB_BASE_REF: ', process.env.GITHUB_BASE_REF);
+console.log('GITHUB_HEAD_REF: ', process.env.GITHUB_HEAD_REF);
+console.log('GITHUB_REF_NAME: ', process.env.GITHUB_REF_NAME);
